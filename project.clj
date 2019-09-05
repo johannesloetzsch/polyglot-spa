76962ede5105be58c03150114986fd41a8978872
Johannes Lötzsch <git@johannesloetzsch.de>
2019-09-05 15:04:47 +0200

    circleci

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject polyglot-spa "0.1.0-SNAPSHOT"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :min-lein-version "2.5.3"
+  :source-paths ["src/clj" "src/cljs"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
@@ -11,21 +18,8 @@
                  [metosin/reitit-frontend "0.3.9"]
                  [prismatic/dommy "1.1.0"]]
 
-  :plugins []
-
-  :min-lein-version "2.5.3"
-
-  :source-paths ["src/clj" "src/cljs"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]]}
+             :prod {}}
 
   :aliases {"dev"  ["with-profile" "dev" "run" "-m" "shadow.cljs.devtools.cli" "watch" "app"]
-            "prod" ["with-profile" "prod" "run" "-m" "shadow.cljs.devtools.cli" "release" "app"]}
-
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.10"]]}
-
-   :prod { }
-   })
+            "prod" ["with-profile" "prod" "run" "-m" "shadow.cljs.devtools.cli" "release" "app"]})
